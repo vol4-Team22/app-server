0e8b0b127f1feb3f23d2eaf5eec2ee79c6d94c8a
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-04-27 15:12:38 +0900

    feat: post handler

---
+++ handler/post.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/go-playground/validator/v10"
+	"mikke-server/domain"
+	"mikke-server/usecase"
+	"net/http"
+)
+
+type PostQuestion struct {
+	Usecase   usecase.PostUsecase
+	Validator *validator.Validate
+}
+
+type PostQuestionsUsecace interface {
+	PostQuestion(ctx context.Context, user_id int, title string, comment string) (*domain.Post, error)
+}
+
+func (p PostQuestion) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	var b struct {
+		Title   string `json:"title" validate:"required"`
+		Comment string `json:"comment" validate:"required"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&b); err != nil {
+		RespondJSON(ctx, w, &ErrResponse{
+			Message: err.Error(),
+		}, http.StatusInternalServerError)
+		return
+	}
+	if err := p.Validator.Struct(b); err != nil {
+		RespondJSON(ctx, w, &ErrResponse{
+			Message: err.Error(),
+		}, http.StatusInternalServerError)
+	}
+	UserID := 7777
+	_, err := p.Usecase.PostQuestion(ctx, UserID, b.Title, b.Comment)
+	if err != nil {
+		RespondJSON(ctx, w, &ErrResponse{
+			Message: err.Error(),
+		}, http.StatusInternalServerError)
+		return
+	}
+	RespondJSON(ctx, w, nil, http.StatusOK)
+}
