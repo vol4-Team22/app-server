b9c993640786b5150495646f313d15078bd7777d
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-06-05 11:00:51 +0900

    fix: repositry

--- internal/database/repositry.go
+++ internal/database/repositry.go
@@ -2,70 +2,19 @@ package database
 
 import (
 	"context"
-	"database/sql"
-	"errors"
-	"fmt"
-	"mikke-server/config"
-	"mikke-server/tools/clock"
-	"time"
-
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/jmoiron/sqlx"
+	"mikke-server/internal/domain"
 )
 
-const ErrCodeMySQLDuplicateEntry = 1062
-
-var (
-	ErrAlreadyEntry = errors.New("duplicate entry")
-)
-
-func New(ctx context.Context, cfg *config.Config) (*sqlx.DB, func(), error) {
-	db, err := sql.Open("mysql",
-		fmt.Sprintf(
-			"%s:%s@tcp(%s:%d)/%s?parseTime=true",
-			cfg.DBUser, cfg.DBPassword, cfg.DBHost, cfg.DBPort, cfg.DBName,
-		),
-	)
-	if err != nil {
-		return nil, func() {}, err
-	}
-	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
-	defer cancel()
-	if err := db.PingContext(ctx); err != nil {
-		return nil, func() { _ = db.Close() }, err
-	}
-	xdb := sqlx.NewDb(db, "mysql")
-	return xdb, func() { _ = db.Close() }, nil
+type PostRepository interface {
+	SendPost(ctx context.Context, db Execer, user_id int, title string, comment string) error
+	ListPosts(ctx context.Context, db Queryer) ([]*domain.Post, error)
+	GetPost(ctx context.Context, db Queryer, postId int) (*domain.Post, error)
 }
 
-type Repository struct {
-	Clocker clock.Clocker
+type ReplyAdder interface {
+	SendReply(ctx context.Context, db Execer, p *domain.Reply) error
 }
 
-type Beginner interface {
-	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error)
-}
-
-type Preparer interface {
-	PreparexContext(ctx context.Context, query string) (*sqlx.Stmt, error)
-}
-
-type Execer interface {
-	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
-	NamedExecContext(ctx context.Context, query string, arg interface{}) (sql.Result, error)
-}
-
-type Queryer interface {
-	Preparer
-	QueryxContext(ctx context.Context, query string, args ...any) (*sqlx.Rows, error)
-	QueryRowxContext(ctx context.Context, query string, args ...any) *sqlx.Row
-	GetContext(ctx context.Context, dest interface{}, query string, args ...any) error
-	SelectContext(ctx context.Context, dest interface{}, query string, args ...any) error
-}
-
-type Interface interface {
-	Beginner
-	Execer
-	Queryer
-	Preparer
+type ReplyLister interface {
+	ListReplies(ctx context.Context, db Queryer, postId domain.PostID) (domain.Replies, error)
 }
